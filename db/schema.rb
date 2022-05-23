1c1c8e243ccc350202730b01e845ef86f6dafd98
Yuma Urakami <yumau717@ezweb.ne.jp>
2022-05-23 06:19:18 +0000

    Scaffold操作の取り消し

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20220516070300) do
 
-  create_table "memos", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
