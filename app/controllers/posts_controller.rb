6e4eb3154528bcb03c5099fb0cf2be0995d18896
Yuma Urakami <yumau717@ezweb.ne.jp>
2022-05-31 02:05:35 +0000

    Lesson07の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,7 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all.order(created_at: 'desc')
     end
+    def show
+        @post = Post.find(params[:id])
+    end
 end
